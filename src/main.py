109e21a6d4b04c2960d917ae211d275a778f51dd
abpa123 <aditya.banwasi@gmail.com>
2025-11-09 12:10:08 -0800

    path

--- src/main.py
+++ src/main.py
@@ -25,8 +25,9 @@ def autonomous_function():
 
     log(("Competition", "competition"), "autonomous_begin")
 
-    robot_position.reset(Position(1200, 1200))
-    reset_heading_to_aim(Position(1400, 1200), FORWARD)
+    robot_position.reset(Position(1200, 1185))
+    reset_heading_to_aim(Position(1480, 1185), FORWARD)
+
 
     matchload.set(True)
 
@@ -36,19 +37,31 @@ def autonomous_function():
 
     wait(1000, MSEC)
 
-    conveyor.spin(FORWARD, FORWARD, FORWARD)
+    conveyor.spin(REVERSE, FORWARD, FORWARD)
 
-    trigger_mover.move(Position(1400, 1200), FORWARD)
+    # trigger_mover.move(Position(1480, 1185), FORWARD)
+    trigger_driver.drive(260)
+    # trigger_driver.drive(-50)
+    # wait(100, MSEC)
+    # trigger_driver.drive(70)
+    matchload.set(False)
+    wait(220, MSEC)
+    matchload.set(True)
 
-    wait(6000, MSEC)
+    wait(200, MSEC)
+    # conveyor.spin(STOP, STOP, STOP)
+    # flap.set(False)
+
+    trigger_mover.move(Position(1200, 1200), REVERSE)
+    conveyor.spin(REVERSE, FORWARD, FORWARD)
+    matchload.set(False)
+    trigger_turner.turn(177, FRAME_HEADING_RELATIVE)
     conveyor.spin(STOP, STOP, STOP)
     flap.set(False)
 
-    trigger_mover.move(Position(800, 1200), REVERSE)
-    matchload.set(False)
-    trigger_turner.turn(180, FRAME_HEADING_RELATIVE)
-
-    conveyor.spin(FORWARD, REVERSE, FORWARD)
+    trigger_mover.move(Position(900, 1200), FORWARD)
+    # trigger_turner.turn(270, FRAME_ABSOLUTE) # do another check
+    conveyor.spin(REVERSE, FORWARD, FORWARD)
 
 
 
